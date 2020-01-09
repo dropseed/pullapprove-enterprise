0f1b9b8067258a4f3c0c8f6ec05a979d7023bd8a
Dave Gaeddert <dave.gaeddert@gmail.com>
2020-01-09 15:09:16 -0600

    Add GitHub action to validate terraform (#60)

--- deploy/aws/provider.tf
+++ deploy/aws/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  version = "~> 1.36"
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "${var.aws_region}"
+  version    = "~> 2.7"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
 }
