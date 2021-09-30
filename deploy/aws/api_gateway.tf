4e233eee30967f4626124833015ba4a822528b5f
Dave Gaeddert <dave.gaeddert@gmail.com>
2021-09-30 14:16:28 -0500

    Redeployment trigger

--- deploy/aws/api_gateway.tf
+++ deploy/aws/api_gateway.tf
@@ -30,6 +30,10 @@ resource "aws_api_gateway_deployment" "pullapprove_deployment" {
   ]
 
   rest_api_id = aws_api_gateway_rest_api.pullapprove_gateway.id
+
+  triggers = {
+    redeployment = sha1(jsonencode(aws_api_gateway_rest_api.pullapprove_gateway.body))
+  }
 }
 
 resource "aws_api_gateway_stage" "pullapprove_gateway_stage" {
